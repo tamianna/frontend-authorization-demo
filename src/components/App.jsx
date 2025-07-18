ed39b6d7221b93f611efb2e477f3e68be3082e47
Tami Hernandez <tamianna.h@gmail.com>
2025-07-18 11:52:37 -0500

    feat: add handleResgistration to register route in function App.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -58,7 +58,7 @@ function App() {
         path="/register"
         element={
           <div className="registerContainer">
-            <Register />
+            <Register handleRegistration={handleRegistration} />
           </div>
         }
       />
