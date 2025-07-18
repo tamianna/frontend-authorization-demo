84956656226199fb602d6b3ec5639baff7dd7d11
Tami Hernandez <tamianna.h@gmail.com>
2025-07-18 14:01:07 -0500

    feat: import useLocation in App.jsx and apply it to function App. add const redirectPath in handleLogin. remove navigate from useEffect. add ProtectedRoute to login & register routes.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,11 @@
 import { useState, useEffect } from "react";
-import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  Navigate,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
 
 import Ducks from "./Ducks";
 import Login from "./Login";
@@ -14,8 +20,8 @@ import "./styles/App.css";
 function App() {
   const [userData, setUserData] = useState({ username: "", email: "" });
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleRegistration = ({
     username,
@@ -44,7 +50,8 @@ function App() {
           setToken(data.jwt);
           setUserData(data.user);
           setIsLoggedIn(true);
-          navigate("/ducks");
+          const redirectPath = location.state?.from?.pathname || "/ducks";
+          navigate(redirectPath);
         }
       })
       .catch(console.error);
@@ -62,7 +69,6 @@ function App() {
       .then(({ username, email }) => {
         setIsLoggedIn(true);
         setUserData({ username, email });
-        navigate("/ducks");
       })
       .catch(console.error);
   }, []);
@@ -88,23 +94,31 @@ function App() {
       <Route
         path="/login"
         element={
-          <div className="loginContainer">
-            <Login handleLogin={handleLogin} />
-          </div>
+          <ProtectedRoute isLoggedIn={isLoggedIn} anonymous>
+            <div className="loginContainer">
+              <Login handleLogin={handleLogin} />
+            </div>
+          </ProtectedRoute>
         }
       />
       <Route
         path="/register"
         element={
-          <div className="registerContainer">
-            <Register handleRegistration={handleRegistration} />
-          </div>
+          <ProtectedRoute isLoggedIn={isLoggedIn} anonymous>
+            <div className="registerContainer">
+              <Register handleRegistration={handleRegistration} />
+            </div>
+          </ProtectedRoute>
         }
       />
       <Route
         path="*"
         element={
-          isLoggedIn ? <Navigate to="/ducks" /> : <Navigate to="/login" />
+          isLoggedIn ? (
+          <Navigate to="/ducks" /> 
+          ) : ( 
+          <Navigate to="/login" />
+          )
         }
       />
     </Routes>
