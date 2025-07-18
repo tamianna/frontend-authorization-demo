c98437273599045d990d859c46649b6792f39149
Tami Hernandez <tamianna.h@gmail.com>
2025-07-18 14:33:27 -0500

    fix: missing /register in BASE_URL in auth.js file. and missing onSubmit in Register.jsx. file.

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -28,7 +28,7 @@ const Register = ({ handleRegistration }) => {
     <div className="register">
       <Logo title={"CryptoDucks"} />
       <p className="register__welcome">Please register.</p>
-      <form className="register__form">
+      <form className="register__form" onSubmit={handleSubmit}>
         <label htmlFor="username">Username:</label>
         <input
           id="username"
